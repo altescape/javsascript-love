a75c7c8ee5b5d6f1a9038b95c1f9b36437f4c335
Michael Watts <hello@michaelwatts.me>
2015-04-26 21:59:36 +0100

    Create shopify-gallery-script.js

---
+++ shopify-gallery-script.js
@@ -0,0 +1,59 @@
+var PHOTOS = (function () {
+  
+  var image_container =  $('.image-container');
+  var photos = $.makeArray(image_container.children());
+
+  var check_img = function () {
+    var photos_array = [];
+    $.each(photos, function (index, value) {
+      if ($(value).is('img')) {
+      	photos_array.push(value);
+      } else {
+      	$(value).remove();
+      }
+    });
+  return photos_array;
+  };
+  
+  var treat_photos = function () {
+    var validated_photos = check_img();
+  	$.each(validated_photos, function (index, value) {
+      var url = value.src;
+      var filename = url.split('/').pop();
+      var name_size = filename.split('.').shift();
+      var name = name_size.split('_').shift();
+      var size = name_size.split('_').pop();
+
+      var new_url = url.replace(size, '1024x1024');
+      $(value).wrap(function() {
+        return "<a href='" + new_url + "' rel='gallery_group' class='fancybox'></a>";
+      });
+    });
+  };
+  
+  var packery = function () {
+    image_container.packery({"gutter": 20});
+  };
+  
+  var fancybox = function () {
+  	$('.fancybox').fancybox();
+  };
+  
+  var fade_in = function () {
+  	image_container.addClass('active');
+  };
+  
+  return {
+    treat_photos: treat_photos,
+    packery: packery,
+    fancybox: fancybox,
+    fade_in: fade_in
+  }
+}());
+    
+$(window).load(function() {
+  PHOTOS.treat_photos();
+  PHOTOS.packery();
+  PHOTOS.fancybox();
+  PHOTOS.fade_in();
+});
